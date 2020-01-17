2351437fe67447adccc6849c0bc95ff05e43c48d
Josh Ford <thisisjoshford@gmail.com>
2020-01-16 17:23:34 -0800

    add total tries and number of wins

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 import { getRandomThrow, checkResult } from './get-random-throw.js';
-const enermyChoice = document.getElementById('enermy-choice');
+const enemyChoice = document.getElementById('enemy-choice');
 //const userChoice USe quaryselector here and then add another variable to declare the selected input by using userChoice.value
 const playButton = document.getElementById('play-game');
+
+let trials = 0;
+let winTrials = 0;
+
 playButton.addEventListener('click', () => {
+    trials++;
     const gameResult = document.getElementById('Result');
+    const totalTrials = document.getElementById('total-trials');
+    const numberOfWins = document.getElementById('number-of-wins');
     const userSelect = document.querySelector('input:checked');
     const userChoice = userSelect.value;
     
-
+    
     console.log(userChoice);
     const computerRandom = getRandomThrow();
     console.log(computerRandom);
-    console.log(gameResult);
-    console.log(checkResult(userChoice, computerRandom));
+    console.log('==============');
     if (checkResult(userChoice, computerRandom) === 'Draw'){
         gameResult.textContent = `Draw, try again!`;
     } else if (checkResult(userChoice, computerRandom) === 'Win'){
-        gameResult.textContent = `You are a winnter`;
+        winTrials++;
+        gameResult.textContent = `You are a winner`;
     } else if (checkResult(userChoice, computerRandom) === 'Loose'){
         gameResult.textContent = `You Lost`;
     }
-    enermyChoice.textContent = computerRandom;
-        
+    enemyChoice.textContent = computerRandom;
+    totalTrials.textContent = trials;
+    numberOfWins.textContent = winTrials;
+    
 });
 
 
